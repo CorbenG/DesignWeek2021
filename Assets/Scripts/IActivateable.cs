9bc9a1ab4522756281db69cf089c8da028d7300b
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2021-03-09 15:48:27 -0500

    Created assets for the word bank system
    Created a scriptable object to hold all the words for a spesific event.
    
    Also made an interface for the words called IActivateable

---
+++ Assets/Scripts/IActivateable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IActivateable
+{
+    string wordName { get; set; }
+
+    void activate();
+
+}
