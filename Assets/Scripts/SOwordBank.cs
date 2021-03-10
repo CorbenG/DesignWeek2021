97a91e4c0976453a6b3018d1e3603966a2b99088
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2021-03-10 16:41:13 -0500

    Started making the character controller and modular event system

--- Assets/Scripts/SOwordBank.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = ("Created Assets/Word Bank"))]
-public class SOwordBank : ScriptableObject
-{
-    public List<SOword> listOfWords;
-}
---
+++ Assets/Scripts/playerInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInput : MonoBehaviour
+{
+    public bool interact { get; private set; }
+    public bool jump { get; private set; }
+    public float moveX { get; private set; }
+
+
+
+}
