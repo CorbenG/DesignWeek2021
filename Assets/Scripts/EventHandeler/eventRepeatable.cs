062c15ed5b5f28b3528873001187a471ce3474ed
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2021-03-11 19:05:38 -0500

    Finished repeatable event script and implemented it

--- Assets/Scripts/EventHandeler/eventRepeatable.cs
+++ Assets/Scripts/EventHandeler/eventRepeatable.cs
@@ -2,17 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class eventRepeatable : MonoBehaviour
+public class eventRepeatable : MonoBehaviour,IActivateable
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource source;
+    public AudioClip[] clips;
+    public string[] subtitle;
+    Subtitles subtitles;
+
+    public int maxNumOfTimesActivated;
+    public int currentNumOfTimesActivated;
+
+    private SpriteRenderer renderer;
+    public Sprite spriteToSwitchTo;
+
+    public bool hasActivated { get; set; }
+
+    private void Awake()
     {
-        
+        renderer = GetComponent<SpriteRenderer>();
+        subtitles = GameObject.Find("Subtitles").GetComponent<Subtitles>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void activate()
     {
-        
+        if (!hasActivated && currentNumOfTimesActivated <= maxNumOfTimesActivated)
+        {
+            playSound();
+            currentNumOfTimesActivated++;
+
+            if (currentNumOfTimesActivated >= maxNumOfTimesActivated) 
+            {
+                renderer.sprite = spriteToSwitchTo;
+                hasActivated = true;
+            }
+        }
     }
+
+    public void playSound()
+    {
+        switch (currentNumOfTimesActivated) 
+        {
+            case 0:
+                source.Stop();
+                source.clip = clips[currentNumOfTimesActivated];
+                subtitles.newText = subtitle[currentNumOfTimesActivated];
+                source.Play();
+                break;
+            case 1:
+                source.Stop();
+                source.clip = clips[currentNumOfTimesActivated];
+                subtitles.newText = subtitle[currentNumOfTimesActivated];
+                source.Play();
+                break;
+            case 2:
+                source.Stop();
+                source.clip = clips[currentNumOfTimesActivated];
+                subtitles.newText = subtitle[currentNumOfTimesActivated];
+                source.Play();
+                break;
+            case 3:
+                source.Stop();
+                source.clip = clips[currentNumOfTimesActivated];
+                subtitles.newText = subtitle[currentNumOfTimesActivated];
+                source.Play();
+                break;
+            case 4:
+                source.Stop();
+                source.clip = clips[currentNumOfTimesActivated];
+                subtitles.newText = subtitle[currentNumOfTimesActivated];
+                source.Play();
+                break;
+            case 5:
+                source.Stop();
+                source.clip = clips[currentNumOfTimesActivated];
+                subtitles.newText = subtitle[currentNumOfTimesActivated];
+                source.Play();
+                break;
+        }
+    }
+
+    
 }
